868f3e91702dc5d308868fd94c9df3d724408804
yxg <1827463397@qq.com>
2024-07-21 21:59:53 +0800

    opengl shader

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -7,6 +7,8 @@
 #include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+#include "Hazel/Render/Shader.h"
+
 namespace Hazel {
 	class HAZEL_API Application
 	{
@@ -30,6 +32,7 @@ namespace Hazel {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 
 		static Application* s_Instance;
 	};
