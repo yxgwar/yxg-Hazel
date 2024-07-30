781d49617dba115a896c707fa299826ee98a262d
yxg <1827463397@qq.com>
2024-07-30 23:18:56 +0800

    transform

--- Hazel/src/Hazel/Render/OrthographicCamera.cpp
+++ Hazel/src/Hazel/Render/OrthographicCamera.cpp
@@ -1,8 +1,6 @@
 #include <hzpch.h>
 #include "OrthographicCamera.h"
 
-#include <glm/gtc/matrix_transform.hpp>
-
 namespace Hazel {
 	Hazel::OrthographicCamera::OrthographicCamera(float left, float right, float bottom, float top)
 		:m_ProjectionMatrix(glm::ortho(left, right, bottom, top, -1.0f, 1.0f)), m_ViewMatrix(1.0f)
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -6,7 +6,8 @@ public:
 	ExampleLayer()
 		: Layer("Example"), 
 		m_Camera(-1.6f, 1.6f, -0.9f, 0.9f),
-		m_CameraPosition(0.0f)
+		m_CameraPosition(0.0f),
+		m_SquarePosition(0.0f)
 	{
 		m_VertexArray.reset(Hazel::VertexArray::Create());
 
@@ -63,6 +64,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -71,7 +73,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -98,13 +100,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -147,13 +150,42 @@ public:
 			m_CameraRotation -= m_CameraRotationSpeed * ts;
 		}
 
+		if (Hazel::Input::IsKeyPressed(HZ_KEY_A)) {
+			m_SquarePosition.x -= m_SquareSpeed * ts;
+		}
+		else if (Hazel::Input::IsKeyPressed(HZ_KEY_D)) {
+			m_SquarePosition.x += m_SquareSpeed * ts;
+		}
+		if (Hazel::Input::IsKeyPressed(HZ_KEY_W)) {
+			m_SquarePosition.y += m_SquareSpeed * ts;
+		}
+		else if (Hazel::Input::IsKeyPressed(HZ_KEY_S)) {
+			m_SquarePosition.y -= m_SquareSpeed	* ts;
+		}
+
 		Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 		Hazel::RenderCommand::Clear();
+
 		m_Camera.SetPosition(m_CameraPosition);
 		m_Camera.SetRotation(m_CameraRotation);
+
 		Hazel::Renderer::BeginScene(m_Camera);
-		Hazel::Renderer::Submit(m_SquareVA, m_Shader2);
-		Hazel::Renderer::Submit(m_VertexArray, m_Shader);
+
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int x = 0; x < 20; x++)
+		{
+			for (int y = 0; y < 20; y++)
+			{
+				glm::vec3 pos = glm::vec3(0.11f * x, 0.11f * y, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Hazel::Renderer::Submit(m_SquareVA, m_Shader2, transform);
+
+			}
+		}
+
+
+		//Hazel::Renderer::Submit(m_VertexArray, m_Shader);
 
 		Hazel::Renderer::EndScene();
 	}
@@ -177,6 +209,9 @@ private:
 	float m_CameraSpeed = 1.0f;
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 20.0f;
+
+	glm::vec3 m_SquarePosition;
+	float m_SquareSpeed = 1.0f;
 };
 
 class Sandbox : public Hazel::Application {
