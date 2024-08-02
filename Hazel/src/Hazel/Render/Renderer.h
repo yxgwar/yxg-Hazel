a108730bec4edc9c17c503ba40f99f901233e7a6
yxg <1827463397@qq.com>
2024-08-02 21:31:59 +0800

    blend

--- Hazel/src/Hazel/Render/Renderer.h
+++ Hazel/src/Hazel/Render/Renderer.h
@@ -9,6 +9,8 @@
 namespace Hazel {
 	class Renderer {
 	public:
+		static void Init();
+
 		static void BeginScene(OrthographicCamera& camera);
 		static void EndScene();
 		static void Submit(const Ref<VertexArray>& vertexArray, const Ref<Shader> shader, const glm::mat4& transform = glm::mat4(1.0f));
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -168,6 +168,7 @@ public:
 		m_TextureShader.reset(Hazel::Shader::Create(textureVertexSrc, textureFragmentSrc));
 
 		m_Texture = Hazel::Texture2D::Create("");
+		m_TextureAlpha = Hazel::Texture2D::Create("");
 
 		std::dynamic_pointer_cast<Hazel::OpenGlShader>(m_TextureShader)->Bind();
 		std::dynamic_pointer_cast<Hazel::OpenGlShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
@@ -236,6 +237,9 @@ public:
 		m_Texture->Bind();
 		Hazel::Renderer::Submit(m_SquareVA, m_TextureShader, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
+		m_TextureAlpha->Bind();
+		Hazel::Renderer::Submit(m_SquareVA, m_TextureShader, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+
 		//Hazel::Renderer::Submit(m_VertexArray, m_Shader);
 
 		Hazel::Renderer::EndScene();
@@ -257,7 +261,7 @@ private:
 	Hazel::Ref<Hazel::Shader> m_Shader2, m_TextureShader;
 	Hazel::Ref<Hazel::VertexArray> m_SquareVA;
 
-	Hazel::Ref<Hazel::Texture2D> m_Texture;
+	Hazel::Ref<Hazel::Texture2D> m_Texture, m_TextureAlpha;
 
 	Hazel::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
