c88fbd7ee60194fb0535c774c2287276bb57d3ec
yxg <1827463397@qq.com>
2024-09-02 21:48:19 +0800

    transform component ui

--- Hazel/src/Hazel/Scene/Scene.cpp
+++ Hazel/src/Hazel/Scene/Scene.cpp
@@ -36,24 +36,24 @@ namespace Hazel {
 			});
 
 		Camera* mainCamera = nullptr;
-		glm::mat4* mainTransform = nullptr;
+		glm::mat4 cameraTransform;
 		auto view = m_Registry.view<TransformComponent, CameraComponent>();
 		for (auto entity : view) {
 			auto& [transform, camera] = view.get<TransformComponent, CameraComponent>(entity);
 			if (camera.Primary) {
 				mainCamera = &camera.Camera;
-				mainTransform = &transform.Transform;
+				cameraTransform = transform.GetTransform();
 				break;
 			}
 		}
 
 		if (mainCamera) {
-			Renderer2D::BeginScene(mainCamera->GetProjection(), *mainTransform);
+			Renderer2D::BeginScene(mainCamera->GetProjection(), cameraTransform);
 			auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
 			for (auto entity : group)
 			{
 				auto& [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
-				Renderer2D::DrawQuad(transform, sprite.Color);
+				Renderer2D::DrawQuad(transform.GetTransform(), sprite.Color);
 			}
 			Renderer2D::EndScene();
 		}
