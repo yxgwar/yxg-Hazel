d28d8ba705f0a0d24bbe114c3e4dc387c1663c37
yxg <1827463397@qq.com>
2024-09-03 19:13:08 +0800

    adding/removing entities and components ui

--- Hazel/src/Hazel/Scene/Entity.h
+++ Hazel/src/Hazel/Scene/Entity.h
@@ -13,7 +13,9 @@ namespace Hazel {
 		template<typename T, typename... Args>
 		T& AddComponent(Args&&... args) {
 			HZ_CORE_ASSERT(!HasComponent<T>(), "Entity already has component!");
-			return m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+			T& component = m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+			m_Scene->OnComponentAdded<T>(*this, component);
+			return component;
 		}
 
 		template<typename T>
@@ -35,6 +37,7 @@ namespace Hazel {
 
 		operator bool() const { return m_EntityHandle != entt::null; }
 		operator uint32_t() const { return static_cast<uint32_t>(m_EntityHandle); }
+		operator entt::entity() const { return m_EntityHandle; }
 
 		bool operator==(const Entity& other) const { return m_EntityHandle == other.m_EntityHandle || m_Scene == other.m_Scene; }
 		bool operator!=(const Entity& other) const { return !(*this == other); }
